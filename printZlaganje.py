e875d984962dc98a9788a179e1500af1b5dd5949
dcrystalj <dcrystalj@gmail.com>
2015-03-20 15:45:01 +0100

    fix spaces

--- printZlaganje.py
+++ printZlaganje.py
@@ -1,16 +1,21 @@
-from Zlaganje import *
+from Zlaganje import * 
+from time import time
 
-with open('Zlaganje.in.txt', 'r+') as f:
+with open('zlaganje.in.txt', 'r+') as f:
     content = f.readlines()
 
 content = content[2:]
 
 
-zlaganje = [[j for j in map(int, content[i].split())] for i in range(300)]
+zlaganje = [[j for j in map(int, content[i].split())] for i in range(int(input()),300)]
 
 for i in zlaganje:
     print(i[0])
+    t1 = time()
     z = Zlaganje(i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[0])
-    z.solve()
+    try:
+        z.solve()
+    except:
+        print(time()-t1)
 
 
